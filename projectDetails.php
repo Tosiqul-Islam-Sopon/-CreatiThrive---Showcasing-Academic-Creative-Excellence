9323830c72751ae4dd01ea1e1613484994b6dc01
Niloy Das <niloydas5215@gmail.com>
2023-12-13 02:19:12 +0600

    Profile2.html file updated and project details and edit page added

---
+++ projectDetails.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    include ('sideBar.php');
+    include ('projectDetails.html'); 
+?>
---
+++ projectEdit.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    include ('sideBar.php');
+    include ('projectEdit.html'); 
+?>
